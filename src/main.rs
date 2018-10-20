ae8b6bb99add4636e9368f637f196c67fd460897
VentGrey <omarpurataf@gmail.com>
2018-10-19 23:02:21 -0500

    Se agregó la función menu

--- src/main.rs
+++ src/main.rs
@@ -27,5 +27,33 @@ struct cliente {
 fn main() {
     let opcion:i32;
 
-    
+    //TODO: Función iniciar Lista
+    //TODO: Ciclo infinito
+    //TODO: Asignar valor a una función (todavia no se como)
+    //TODO: hacer un switch
+    //TODO: Excepción en caso de que el switch no jale
 }
+
+fn iniciar_lista() -> () {
+    /* Sobres, vamos a calar los iteradores de Rust aquí */
+    //TODO: Iterador sobre un arreglo de estructuras
+}
+
+fn menu() -> i32 {
+    let s:String;
+
+    let c:i32;
+
+    println!("1- Introducir un cliente");
+    println!("2- Eliminar un cliente");
+    println!("3- Listar los clientes");
+    println!("4- Buscar dentro del archivo");
+    println!("5- Guardar archivo");
+    println!("6- Cargar archivo");
+    println!("7- Ordenar archivo por nombre");
+    println!("8- Salir");
+    println!("9- Acerca de:");
+
+    return c
+}
+
