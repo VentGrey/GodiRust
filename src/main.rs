2268efe6d41e08c18652bf72d69376867f3c3fcf
VentGrey <omarpurataf@gmail.com>
2018-10-19 20:43:20 -0500

    Se agregó la estructura necesaria para el cliente

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
+//Constante para limitar el número de entradas
+const MAX:i64 = 50;
+
+struct cliente {
+    ID: String,
+    nombre: String,
+    telefono: String,
+    dia: u64,
+    mes: u64,
+    anio: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
