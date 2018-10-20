ddf9cd2d6992c1759e97e95aeaf6658dc8320c78
VentGrey <omarpurataf@gmail.com>
2018-10-19 21:59:50 -0500

    se terminaron los comentarios para funciones

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use std::io;
 
-//Constante para limitar el número de entradas
-const MAX:i64 = 50;
-
 struct cliente {
     ID: String,
     nombre: String,
@@ -10,7 +7,20 @@ struct cliente {
     dia: u64,
     mes: u64,
     anio: u64,
-} cliente[MAX];
+}
+
+// Estas son las funciones básicas para que jale el programa
+// Funciones tipo void:
+// * Cargar
+// * Guardar
+// * Buscar
+// * Muestra (parámetro: int i)
+// * LeerCadena (parametros: puntero char indic, s e int contador)
+// * Borrar
+// * Intro
+// * Mostrar
+// * Iniciar lista
+// * buscar libre (parámetro: void)
 
 fn main() {
 
